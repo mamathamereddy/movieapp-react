7242ccd05a2acbd77b3d572ab3c7da86330a3580
mamathamereddy <mamathavangala1@gmail.com>
2022-12-22 16:33:37 +0100

    adding titile and search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+//key: 74a8767f
 
-function App() {
-  return <h1>hello</h1>;
-}
+const API_URL = "http://www.omdbapi.com?apikey=74a8767f";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Movie Land</h1>
+
+      <div className="search">
+        <input placeholder="serch for a movie" value="" onChange={() => {}} />
+        <img src={SearchIcon} alt="serach" onClick={() => {}} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
